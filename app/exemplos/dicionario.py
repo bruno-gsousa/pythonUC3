9445704f97971f086d11aee20835b3625456f7cb
bruno-gsousa <bruno.gsousa.senac@gmail.com>
2026-06-15 22:01:13 -0300

    tutorial_django

--- app/exemplos/dicionario.py
+++ app/exemplos/dicionario.py
@@ -1,11 +1,11 @@
 pessoa = {
     "nome": "Ana",
-    "cpf" : "540.546.455-45",
+    "cpf": "540.546.455-45",
     "telefone": 2199556656
 }
 print(pessoa)
 print(pessoa["cpf"])
-pessoa["nome"] = "Luiz" # alterei o valor
+pessoa["nome"] = "Luiz"# alterei o valor
 print(pessoa["nome"])
 
 for chave, valor in pessoa.items():
